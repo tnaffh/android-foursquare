a0285c8debc3d826ddd3af450dfa378decdb838a
letrunghieu.cse09@gmail.com <letrunghieu.cse09@gmail.com@94f939b7-f677-ebe1-58de-f0aa763075eb>
2011-09-25 02:48:00 +0000

    Create Login Activity Initiate the Interface for communicating with FourSquare

---
+++ trunk/GetFourSquare/src/org/android/olp/foursquare/communicate/IFourSquareAPI.java
@@ -0,0 +1,14 @@
+package org.android.olp.foursquare.communicate;
+
+/**
+ * Please implement this Interface in you class to communicate with others.
+ * My Activities will just use this interface instead of your whole class.
+ * 
+ * OUR FOURSQUARE API
+ * 
+ * @author trunghieu
+ *
+ */
+public interface IFourSquareAPI {
+
+}
